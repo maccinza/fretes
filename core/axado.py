f4e034074a67a962bfc89f6fe821a5ad25baf645
maccinza <maccinza@gmail.com>
2015-05-20 01:40:24 -0300

    Inicio de documentacao adequada do codigo

--- core/axado.py
+++ core/axado.py
@@ -1,14 +1,31 @@
 # -*- coding: utf-8 -*-
 __author__ = 'infante'
 
+u"""Este módulo realiza leitura da base de informações e realiza calculo de fretes a partir de parâmetros de entrada
+que informam origem, destino, preço de nota e peso de um pacote.
+"""
+
+# importa modulo csv para ler os arquivos de base de dados, os para realizar tratamento de paths,
+# sys para obter parametros de entrada do programa e a funcao ceil do modulo math para calulo do arredontamento
 import csv
 import os
 import sys
 from math import ceil
 
+# define caminho para diretorio base do projeto e caminho para o diretorio de tabelas
 DIR_BASE = os.path.dirname(os.path.dirname(__file__))
 DIR_TABELAS = os.path.join(DIR_BASE, 'tabelas')
 
+# Inicializa variaveis globais do programa e valores default
+# ROTAS: dicionario com as informacoes coletadas dos arquivos rotas.csv e rotas.tsv
+# PRECO_POR_KG: dicionario com as informacoes coletadas dos arquivos preco_por_kg.csv e preco_por_kg.tsv
+# SEPARADOR_PADRAO: define um separador padrao a ser utilizado na leitura dos arquivos de dados
+# CSV: tupla que relaciona extensao csv com o caractere a ser utilizado como separador no arquivo (csv)
+# TSV: tupla que relaciona extensao tsv com o caractere a ser utilizado como separador no arquivo (tsv)
+# ICMS_FIXO: valor de icms considerado para os calculos dos fretes de tabela
+# TOLERANCIA: float indicando a tolerancia de diferenca entre os valores limites superiores de uma faixa e o peso
+# CASAS_DECIMAIS: inteiro que define o numero de casas decimais utilizadas no arredontamento
+# RESULTADOS: dicionario que armazena os resultados dos calculos de frete para todas as tabelas
 ROTAS = {}
 PRECO_POR_KG = {}
 SEPARADOR_PADRAO = ','
@@ -21,10 +38,16 @@ RESULTADOS = {}
 
 
 def arredonda_para_cima(valor, casas_decimais):
+    u"""Calcula o arredontamento para cima do valor utilizando o número de casas decimais informadas"""
     return ceil((10**casas_decimais * valor)) / float(10**casas_decimais)
 
 
 def checa_numerico(valor):
+    u"""Verifica se a string valor contém um valor numérico (int, long, float)"""
+
+    # Tenta separar a string no caractere ponto ('.', para tratar float) e verifica se caracteres resultantes da
+    # separacao sao todos digitos (0 a 9), montando uma lista de booleanos (um para cada caractere avaliado).
+    # Em seguida retorna a reducao da lista aplicando um 'AND' dois a dois dos booleanos
     return reduce(lambda x, y: x and y, [num.isdigit() for num in valor.split(".")])
 
 
